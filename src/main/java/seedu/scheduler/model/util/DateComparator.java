b32c322afed6599c106261f91c407df512eef6e8
Rui Zhi <gohruizhi@u.nus.edu>
2019-11-07 21:50:10 +0800

    Sort dates for schedules

--- src/main/java/seedu/scheduler/model/util/DateComparator.java
+++ src/main/java/seedu/scheduler/model/util/DateComparator.java
@@ -5,7 +5,18 @@ import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.Date;
 
+/**
+ * Class to compare between 2 dates in string.
+ */
 public class DateComparator implements Comparator<String> {
+
+    /**
+     * Compares 2 dates in string.
+     * @param dateStringA
+     * @param dateStringB
+     * @return Positive int signifies dateStringA > dateString B. Negative int signifies dateStringA < dateString B.
+     * Zero is returned when both strings are equal or when there is an exception.
+     */
     public int compare(String dateStringA, String dateStringB) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         try {
